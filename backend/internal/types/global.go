6f1359f5680d3693d02b27e69180b419e74c3cd3
Mally <diflamez@gmail.com>
2026-01-20 22:13:34 +0000

    feat: add CreateUserParams type for user creation with necessary fields

---
+++ backend/internal/types/global.go
@@ -0,0 +1,20 @@
+package types
+
+import "github.com/google/uuid"
+
+// Role represents the role of a user in the system.
+// Setting type like this to allow for easier changes in the future if needed.
+type Role string
+
+// CreateUserParams holds parameters for creating a new user.
+// Can't just use repo.CreateUserParams because the password needs to be plain text here.
+type CreateUserParams struct {
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	LicenceID uuid.UUID `json:"licence_id"`
+	Role      Role      `json:"role"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	IsActive  bool      `json:"is_active"`
+}
