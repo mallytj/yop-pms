3e46c94c4c9a09d739a8b50ce5948178b7d0a89b
Mally <diflamez@gmail.com>
2026-03-12 11:19:07 +0000

    feat: initialise svelte

---
+++ web/svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from "@sveltejs/adapter-auto";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter(),
+    alias: {
+      $lib: "src/lib",
+      $components: "src/lib/components",
+      $helpers: "src/lib/helpers",
+      $stores: "src/lib/stores",
+      $types: "src/lib/types",
+      $actions: "src/lib/actions",
+    },
+  },
+  vitePlugin: {
+    dynamicCompileOptions: ({ filename }) => ({
+      runes: !filename.includes("node_modules"),
+    }),
+  },
+};
+
+export default config;
